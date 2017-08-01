21b0eb61be3188fd704cde25b20378a8d0235f73
xubinbin1024 <596928539@qq.com>
2017-08-01 19:29:54 +0800

    参数非空判断

--- src/com/http/servlet/MyServlet.java
+++ src/com/http/servlet/MyServlet.java
@@ -18,7 +18,7 @@ import com.http.bean.User;
  */
 public class MyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,45 +27,63 @@ public class MyServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String nameTemp = request.getParameter("name");
-		String name = new String(nameTemp.getBytes("iso-8859-1"),"utf-8");
-		String age = request.getParameter("age");
-		
-		response.setContentType("text/html; charset=utf-8");
-		PrintWriter out =  response.getWriter();
-		out.print("name = "+name+"; age = " + age); 
-		System.out.println("name = "+name+"; age = " + age);
-	}
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String nameTemp = request.getParameter("name");
+        String age = request.getParameter("age");
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html; charset=utf-8");
+        OutputStream outputStream = response.getOutputStream();
+        StringBuilder backInfo = new StringBuilder();//反馈信息
+        if (nameTemp == null) {
+            backInfo.append("请输入用户名！");
+        }
+        String name = new String(nameTemp.getBytes("iso-8859-1"), "utf-8");
 
-		response.setContentType("application/json; charset=utf-8");  
-        response.setCharacterEncoding("UTF-8");  
-        
-		String nameTemp = request.getParameter("name");
-		String name = new String(nameTemp.getBytes("iso-8859-1"),"utf-8");
-		String age = request.getParameter("age");
-		
-		OutputStream out =  response.getOutputStream();
-	    String userJson = JSON.toJSONString(buildUserDO());  
-		out.write(userJson.getBytes("UTF-8"));  
-		System.out.println("name = "+name+"; age = " + age);
-	}
-	  private User buildUserDO(){
-	        User user = new User();  
-	        user.setName("王二麻子");  
-	        user.setPassword("sdgsgd");  
-	        user.setAge(11);  
-	        user.setBirthday(new Date());  
-	          
-	        return user;  
-	    }  
+        if (age == null) {
+            backInfo.append("请输入年龄");
+        }
+        outputStream.write(backInfo.toString().getBytes("UTF-8"));
+//        PrintWriter out = response.getWriter();
+//        out.print("name = " + name + "; age = " + age);
+//        System.out.println("name = " + name + "; age = " + age);
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String nameTemp = request.getParameter("name");
+        String age = request.getParameter("age");
+
+        response.setContentType("text/html; charset=utf-8");
+        OutputStream outputStream = response.getOutputStream();
+        StringBuilder backInfo = new StringBuilder();//反馈信息
+        if (nameTemp == null) {
+            backInfo.append("请输入用户名！");
+        }
+        if (age == null) {
+            backInfo.append("请输入年龄");
+        }
+//        String userJson = JSON.toJSONString(buildUserDO());
+        String userJson = "sadadads";
+        outputStream.write(userJson.getBytes("UTF-8"));
+        outputStream.write(backInfo.toString().getBytes("UTF-8"));
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    private User buildUserDO() {
+        User user = new User();
+        user.setName("王二麻子");
+        user.setPassword("sdgsgd");
+        user.setAge(11);
+        user.setBirthday(new Date());
+
+        return user;
+    }
 
 }
